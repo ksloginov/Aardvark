e008d8f73cc5a5c539977a40335166c1fa29760e
Konstantin Loginov <kloginov@squareup.com>
2026-01-22 15:12:56 +0100

    Resolving the module 'Aardvark' has no member named 'addDefaultBugReportingGestureWithEmailBugReporter' issue

--- Sources/AardvarkMailUISwift/Aardvark+EmailBugReporting.swift
+++ Sources/AardvarkMailUISwift/Aardvark+EmailBugReporting.swift
@@ -5,7 +5,7 @@
 //  you may not use this file except in compliance with the License.
 //  You may obtain a copy of the License at
 //
-//    http://www.apache.org/licenses/LICENSE-2.0
+//    http://www.apache.org/licenses/LICENSE-2.0
 //
 //  Unless required by applicable law or agreed to in writing, software
 //  distributed under the License is distributed on an "AS IS" BASIS,
@@ -19,6 +19,9 @@ import Aardvark
 #if SWIFT_PACKAGE
 import AardvarkSwift
 import AardvarkMailUI
+
+/// Re-export the Aardvark class so consumers can use `Aardvark.methodName()` syntax
+public typealias Aardvark = AardvarkSwift.Aardvark
 #endif
 
 extension Aardvark {
@@ -32,7 +35,7 @@ extension Aardvark {
         let logStore = ARKLogDistributor.default().defaultLogStore
         let bugReporter = ARKEmailBugReporter(emailAddress: emailAddress, logStore: logStore)
 
-        let gestureRecognizer = Aardvark.add(
+        let gestureRecognizer = Self.add(
             bugReporter: bugReporter,
             triggeringGestureRecognizerClass: UILongPressGestureRecognizer.self
         )
